c2596c6bc8026f83a20d578d07731aa98e5f0998
dipesh <kcdipesh429@gmail.com>
2022-01-08 16:14:25 +0545

    style:tailwind and postcss config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
